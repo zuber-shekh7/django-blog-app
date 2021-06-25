0eec2a2cb4621526e2b730f7f2eca9c1a8824380
Zuber Shekh <zuber.shekh7@gmail.com>
2021-06-25 23:16:08 +0530

    Update update post page

--- blog/forms.py
+++ blog/forms.py
@@ -28,6 +28,23 @@ class PostCreateForm(forms.ModelForm):
 
 
 class PostUpdateForm(forms.ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Enter title'
+            }
+        )
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'placeholder': 'Write...',
+                'rows': 7,
+            },
+            
+        ),
+        required= False
+    )
     
     class Meta:
         model = Post
